81fad8cf7ba6e5335cf650c21915566b0cdd1f5b
Asier <29598090+AsierDev@users.noreply.github.com>
2020-08-13 20:06:42 +0200

    Set moongoose and express

---
+++ api-notes/index.js
@@ -0,0 +1,39 @@
+// require('dotenv').config()
+import {} from 'dotenv/config'
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import mongoose from "mongoose";
+import schema from "./schema";
+
+const host = process.env.MONGO_HOST
+const MongoPort = process.env.MONGO_PORT
+const database = process.env.MONGO_DB
+
+console.log('process.env => ', process.env)
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://${host}:${MongoPort}/${database}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const app = express();
+const PORT = 4300;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Notes API v1"
+  });
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
